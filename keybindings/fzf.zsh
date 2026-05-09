9f5aa3bffb6a7b627757cb3cf7a6da2c525857cf
froostyroboot <196620062+frostyroboot@users.noreply.github.com>
2026-05-08 22:30:13 -0500

    mas errores

--- keybindings/fzf.zsh
+++ keybindings/fzf.zsh
@@ -1,1 +1,7 @@
 bindkey '^F' fzf-file-widget
+
+# Remap fzf keybindings: Solo Ctrl+F, sin Ctrl+T/Ctrl+R
+bindkey -r '^T'  # Remueve Ctrl+T (fzf por defecto)
+bindkey -r '^R'  # Remueve Ctrl+R (historial fzf, usas autuin en su lugar)
+
+# Nota: Ctrl+F queda asignado en keybindings/fzf.zsh via 'bindkey "^F" fzf-file-widget'
