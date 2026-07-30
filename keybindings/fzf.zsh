6a107ffe7ff583dde6e26f732d0724a5daff4949
froostyroboot <196620062+frostyroboot@users.noreply.github.com>
2026-07-29 21:43:41 -0500

    fix(keybindings): limpiar escape sequences CSI/OSC/APC/kitty en widgets fzf
    - Añadir | tail -n1 | sed -E para eliminar secuencias de escape en selección
    - Patrón mejorado: cubre CSI (ESC[), OSC (ESC]), APC (ESC_), kitty (ESC_), ST (ESC\)
    - Solo asignar LBUFFER si exit code == 0 (ENTER, no ESC)

--- keybindings/fzf.zsh
+++ keybindings/fzf.zsh
@@ -12,7 +12,8 @@ export FZF_PREVIEW_WRAPPER="${FZF_PREVIEW_DIR:-$ZSH_CONFIG/preview}/fz-wrapper.s
 # Ctrl-F: seleccionar archivo(s) y pegar al prompt
 fz_file_widget() {
     local selected
-    selected="$($FZF_PREVIEW_WRAPPER < <(fd -t f . 2>/dev/null) --query="$LBUFFER" --select-1 --exit-0 -m 2>/dev/null)"
+    # Capturar stdout, tomar última línea, limpiar TODAS las escape sequences (CSI, OSC, APC, kitty)
+    selected="$($FZF_PREVIEW_WRAPPER < <(fd -t f . 2>/dev/null) --query="$LBUFFER" --select-1 --exit-0 -m 2>/dev/null | tail -n1 | sed -E 's/\x1b[][()P\\^_][^[:cntrl:]]*([\x07\x1b\\]|\x1b\\)//g')"
     local ret=$?
     if [[ $ret -eq 0 && -n "$selected" ]]; then
         LBUFFER="$selected"
@@ -25,7 +26,7 @@ bindkey '^F' fz_file_widget
 # Ctrl-T: seleccionar directorio y pegar `cd <dir>` al prompt
 fz_cd_widget() {
     local selected
-    selected="$($FZF_PREVIEW_WRAPPER < <(fd -t d --hidden --follow --exclude .git) 2>/dev/null)"
+    selected="$($FZF_PREVIEW_WRAPPER < <(fd -t d --hidden --follow --exclude .git) 2>/dev/null | tail -n1 | sed -E 's/\x1b[][()P\\^_][^[:cntrl:]]*([\x07\x1b\\]|\x1b\\)//g')"
     local ret=$?
     if [[ $ret -eq 0 && -n "$selected" ]]; then
         LBUFFER="cd ${(q)selected}"
