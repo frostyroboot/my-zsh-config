d10ccae42e77287000f566dd035d7ed10398ab18
froostyroboot <196620062+frostyroboot@users.noreply.github.com>
2026-07-29 22:03:44 -0500

    fix(keybindings): corregir sintaxis fd para Ctrl+T
    - fd -t d requiere patrón '.' antes del path: 'fd -t d . "$HOME"'
    - Eliminar --hidden --follow --exclude .git que causaban salida vacía
    - Simplificar a 'fd -t d . "$HOME"' (funciona igual que para archivos)

--- keybindings/fzf.zsh
+++ keybindings/fzf.zsh
@@ -26,7 +26,7 @@ bindkey '^F' fz_file_widget
 # Ctrl-T: seleccionar directorio y pegar `cd <dir>` al prompt
 fz_cd_widget() {
     local selected
-    selected="$($FZF_PREVIEW_WRAPPER < <(fd -t d --hidden --follow --exclude .git "$HOME" 2>/dev/null) | tail -n1 | sed -E 's/\x1b[][()P\\^_][^[:cntrl:]]*([\x07\x1b\\]|\x1b\\)//g')"
+    selected="$($FZF_PREVIEW_WRAPPER < <(fd -t d . "$HOME" 2>/dev/null) 2>/dev/null | tail -n1 | sed -E 's/\x1b[][()P\\^_][^[:cntrl:]]*([\x07\x1b\\]|\x1b\\)//g')"
     local ret=$?
     if [[ $ret -eq 0 && -n "$selected" ]]; then
         LBUFFER="cd ${(q)selected}"
